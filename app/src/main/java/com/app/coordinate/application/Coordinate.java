b111a3620caf184def7029f8ee9cd7bff39d0f7c
umeshbsa <umesh.bsa@gmail.com>
2018-12-17 16:18:24 +0530

    Coordinate layout with toolbar hide/show

---
+++ app/src/main/java/com/app/coordinate/application/Coordinate.java
@@ -0,0 +1,13 @@
+package com.app.coordinate.application;
+
+import android.app.Application;
+
+public class Coordinate extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
